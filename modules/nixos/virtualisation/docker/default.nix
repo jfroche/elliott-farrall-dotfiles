9b4d812cf2143e6c7269ce31acee6a8eca9f1c1b
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2025-01-27 12:30:00 +0000

    fix: fixed bug where docker-login service failed on startup

--- modules/nixos/virtualisation/docker/default.nix
+++ modules/nixos/virtualisation/docker/default.nix
@@ -17,20 +17,22 @@ in
 
     systemd.services.docker-login = lib.mkIf cfg.docker.enable {
       description = "Docker Login Service";
-      after = [ "network.target" ];
+      after = [ "network-online.target" ];
       script = ''
         ${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-password.path} | ${pkgs.docker}/bin/docker login --username $(${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-username.path}) --password-stdin
       '';
       wantedBy = [ "multi-user.target" ];
+      serviceConfig.Restart = "on-failure";
     };
 
     systemd.services.podman-login = lib.mkIf cfg.podman.enable {
       description = "Podman Login Service";
-      after = [ "network.target" ];
+      after = [ "network-online.target" ];
       script = ''
         ${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-password.path} | ${pkgs.podman}/bin/podman login --username $(${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-username.path}) --password-stdin
       '';
       wantedBy = [ "multi-user.target" ];
+      serviceConfig.Restart = "on-failure";
     };
 
     virtualisation.podman.dockerSocket.enable = cfg.podman.enable;
