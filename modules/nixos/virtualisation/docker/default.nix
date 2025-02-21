607d34546648a93b3e265b6b08d1c571d29d3e69
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2025-02-21 10:53:01 +0000

    feat: fix docker-login unit dependencies

--- modules/nixos/virtualisation/docker/default.nix
+++ modules/nixos/virtualisation/docker/default.nix
@@ -18,6 +18,7 @@ in
     systemd.services.docker-login = lib.mkIf cfg.docker.enable {
       description = "Docker Login Service";
       after = [ "network-online.target" ];
+      requires = [ "network-online.target" ];
       script = ''
         ${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-password.path} | ${pkgs.docker}/bin/docker login --username $(${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-username.path}) --password-stdin
       '';
@@ -28,6 +29,7 @@ in
     systemd.services.podman-login = lib.mkIf cfg.podman.enable {
       description = "Podman Login Service";
       after = [ "network-online.target" ];
+      requires = [ "network-online.target" ];
       script = ''
         ${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-password.path} | ${pkgs.podman}/bin/podman login --username $(${pkgs.coreutils}/bin/cat ${config.age.secrets.docker-username.path}) --password-stdin
       '';
