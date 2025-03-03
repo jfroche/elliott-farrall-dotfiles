1b748b0f8103bdaa026f1cc812af37cdd819abcc
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2025-03-03 20:34:22 +0000

    fix: mathematica package

--- modules/home/programs/mathematica/default.nix
+++ modules/home/programs/mathematica/default.nix
@@ -19,14 +19,14 @@ in
         name = "mathematica";
         paths = [ pkgs.mathematica ];
         buildInputs = [ pkgs.makeWrapper ];
-        # postBuild = ''
-        #   # wrapProgram $out/libexec/Mathematica/Executables/Mathematica \
-        #   #   --set MATHEMATICA_USERBASE ${config.xdg.configHome}/Mathematica
-        #   install -v ${pkgs.mathematica}/share/applications/com.wolfram.Mathematica.14.0.desktop $out/share/applications/com.wolfram.Mathematica.14.0.desktop
-        #   substituteInPlace $out/share/applications/com.wolfram.Mathematica.14.0.desktop \
-        #     --replace "Name=Mathematica 14.0" "Name=Mathematica" \
-        #     --replace "Exec=${pkgs.mathematica}/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F" "Exec=$out/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F"
-        # '';
+        postBuild = ''
+          # wrapProgram $out/libexec/Mathematica/Executables/Mathematica \
+          #   --set MATHEMATICA_USERBASE ${config.xdg.configHome}/Mathematica
+          install -v ${pkgs.mathematica}/share/applications/com.wolfram.Mathematica.14.0.desktop $out/share/applications/com.wolfram.Mathematica.14.0.desktop
+          substituteInPlace $out/share/applications/com.wolfram.Mathematica.14.0.desktop \
+            --replace "Name=Mathematica 14.0" "Name=Mathematica" \
+            --replace "Exec=${pkgs.mathematica}/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F" "Exec=$out/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F"
+        '';
       })
     ];
   };
