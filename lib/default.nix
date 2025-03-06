42f0705a7014d5772d5efc3c279a415c853bd295
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2025-03-06 21:46:02 +0000

    refactor: remove redundant libs

--- lib/default.nix
+++ lib/default.nix
@@ -11,27 +11,5 @@
   # returns: string       unit name
   mkMountName = path: lib.strings.removePrefix "-" "${builtins.replaceStrings [ "/" ] [ "-" ] path}";
 
-  # Make home activation script to link files
-  #
-  # src: string           source directory
-  # dest: string          destination directory
-  #
-  # returns: DAG entry    script
-  mkLinkScript = src: dest: lib.home-manager.hm.dag.entryAfter [ "linkGeneration" ] ''
-    if [ -e ${dest} ]; then
-      run rm -r ${dest}
-    fi
-    if [ ! -e ${src} ]; then
-      run mkdir -p ${src}
-    fi
-    run ln -s ${src} ${dest}
-  '';
-  mkLinkScript' = src: dest: lib.home-manager.hm.dag.entryAfter [ "linkGeneration" ] ''
-    if [ -e ${dest} ]; then
-      run rm -r ${dest}
-    fi
-    run ln -s ${src} ${dest}
-  '';
-
   mkDefaultApplications = app: mimes: lib.genAttrs mimes (_mime: app);
 }
