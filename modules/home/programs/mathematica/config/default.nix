5c99ad68cf027821832e0968063655fe6f83eee4
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2025-03-06 21:10:47 +0000

    feat: various changes to mathematica

--- modules/home/programs/mathematica/config/default.nix
+++ modules/home/programs/mathematica/config/default.nix
@@ -9,38 +9,21 @@ let
 in
 {
   config = lib.mkIf enable {
-    xdg.configFile."Mathematica/Kernel/init.m" = {
-      text = ''
-        With[{dir = $UserDocumentsDirectory <> "/Wolfram Mathematica"},
-          If[DirectoryQ[dir], DeleteDirectory[dir]]
-        ]
-      '';
-      force = true;
-    };
+    home.file.".Mathematica/FrontEnd/init.m".text = ''
+      SetOptions[$FrontEnd,
+        VersionsLaunched->{"14.0.0"},
 
-    xdg.configFile."Mathematica/FrontEnd/init.m" = {
-      text = ''
-        SetOptions[$FrontEnd,
-          VersionsLaunched->{"14.0.0"},
+        DefaultStyleDefinitions -> "${config.home.homeDirectory}/.Mathematica/SystemFiles/FrontEnd/StyleSheets/Catppuccin.nb",
 
-          NotebookBrowseDirectory -> "${config.xdg.configHome}/Mathematica/SystemFiles/FrontEnd/StyleSheets",
-          DefaultStyleDefinitions -> "${config.xdg.configHome}/Mathematica/SystemFiles/FrontEnd/StyleSheets/Catppuccin.nb",
+        WindowToolbars -> {},
+        ShowPredictiveInterface -> False,
 
-          WindowToolbars -> {},
-          ShowPredictiveInterface -> False,
+        NotebookAutoSave -> True,
+        ClosingAutoSave -> True,
 
-          NotebookAutoSave -> True,
-          ClosingAutoSave -> True,
-
-          PrivateFrontEndOptions -> {ShowAtStartup -> NewDocument},
-          PrivateNotebookOptions -> {FinalWindowPrompt -> False}
-        ]
-      '';
-      force = true;
-    };
-
-    home.activation = {
-      linkWolfram = lib.internal.mkLinkScript "${config.xdg.configHome}/Wolfram" "${config.home.homeDirectory}/.Wolfram";
-    };
+        PrivateFrontEndOptions -> {ShowAtStartup -> NewDocument},
+        PrivateNotebookOptions -> {FinalWindowPrompt -> False}
+      ]
+    '';
   };
 }
--- modules/home/programs/mathematica/default.nix
+++ modules/home/programs/mathematica/default.nix
@@ -7,6 +7,18 @@
 let
   cfg = config.programs.mathematica;
   inherit (cfg) enable;
+
+  package = pkgs.symlinkJoin {
+    name = "mathematica";
+    paths = [ pkgs.mathematica ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      install -v ${pkgs.mathematica}/share/applications/com.wolfram.Mathematica.14.0.desktop $out/share/applications/com.wolfram.Mathematica.14.0.desktop
+      substituteInPlace $out/share/applications/com.wolfram.Mathematica.14.0.desktop \
+        --replace "Name=Mathematica 14.0" "Name=Mathematica" \
+        --replace "Exec=${pkgs.mathematica}/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F" "Exec=$out/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F"
+    '';
+  };
 in
 {
   options = {
@@ -14,20 +26,6 @@ in
   };
 
   config = lib.mkIf enable {
-    home.packages = [
-      (pkgs.symlinkJoin {
-        name = "mathematica";
-        paths = [ pkgs.mathematica ];
-        buildInputs = [ pkgs.makeWrapper ];
-        # postBuild = ''
-        #   wrapProgram $out/libexec/Mathematica/Executables/Mathematica \
-        #     --set MATHEMATICA_USERBASE ${config.xdg.configHome}/Mathematica
-        #   install -v ${pkgs.mathematica}/share/applications/com.wolfram.Mathematica.14.0.desktop $out/share/applications/com.wolfram.Mathematica.14.0.desktop
-        #   substituteInPlace $out/share/applications/com.wolfram.Mathematica.14.0.desktop \
-        #     --replace "Name=Mathematica 14.0" "Name=Mathematica" \
-        #     --replace "Exec=${pkgs.mathematica}/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F" "Exec=$out/libexec/Mathematica/Executables/Mathematica --name com.wolfram.Mathematica.14.0 %F"
-        # '';
-      })
-    ];
+    home.packages = [ package ];
   };
 }
