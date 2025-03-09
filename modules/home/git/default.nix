5a5f94726d7bfc491eb4af55598eef320adac47f
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-03-09 20:06:02 +0000

    style: remove redundant git config

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,29 +1,17 @@
 { config
-, pkgs
 , ...
 }:
 
-let
-  pubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLh0wL2CDMQL0iVtyC2V+RPNJiWoO3oLBsjgYW8fmgt"; # Doesn't match saved public key
-in
 {
   age.secrets = {
     "github/auth".file = ./auth.age;
     "github/sign".file = ./sign.age;
-    "github/sign".path = "${config.home.homeDirectory}/.ssh/keys/github/sign";
   };
 
   programs.git = {
     enable = true;
     userName = "elliott-farrall";
     userEmail = "108588212+elliott-farrall@users.noreply.github.com";
-
-    extraConfig = {
-      commit.gpgsign = true;
-      gpg.format = "ssh";
-      gpg.ssh.allowedSignersFile = "${pkgs.writeText "allowed_signers" "* ${pubKey}"}";
-      user.signingkey = pubKey;
-    };
   };
 
   programs.gh = {
